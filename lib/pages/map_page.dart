9578fe41c8cbc0e3a2e328909ea8397f1717077b
hkathe <2533982015@mail.utec.edu.sv>
2022-05-23 22:49:12 -0600

    feat: get dynamyc locations from firestore

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() => runApp(const MapPage());
 
@@ -12,31 +13,6 @@ class MapPage extends StatefulWidget {
 
 class _MapPageState extends State<MapPage> {
   late GoogleMapController mapController;
-  var markers = {
-    Marker(
-        markerId: const MarkerId('1'),
-        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueAzure),
-        infoWindow: const InfoWindow(
-            title: 'Suc, salvador del mundo', snippet: 'MovilSV'),
-        position: const LatLng(13.700720818864918, -89.22409534458622)),
-    Marker(
-        markerId: const MarkerId('2'),
-        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
-        infoWindow: const InfoWindow(title: 'Suc, arce', snippet: 'MovilSV'),
-        position: const LatLng(13.70001885108788, -89.198177160561970)),
-    Marker(
-        markerId: const MarkerId('3'),
-        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueOrange),
-        infoWindow: const InfoWindow(
-            title: 'Suc, centro de soyapango', snippet: 'MovilSV'),
-        position: const LatLng(13.694009176433218, -89.13702875728684)),
-    Marker(
-        markerId: const MarkerId('4'),
-        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
-        infoWindow:
-            const InfoWindow(title: 'Suc, san jacinto', snippet: 'MovilSV'),
-        position: const LatLng(13.687889969755282, -89.19026354891311)),
-  };
 
   final _center =
       const LatLng(13.695478508868032, -89.21456579946783); //san_salvador
@@ -47,18 +23,38 @@ class _MapPageState extends State<MapPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          title: const Text('Maps App'),
-          backgroundColor: Colors.amber.shade700),
-      body: GoogleMap(
-        onMapCreated: _onMapCreated,
-        initialCameraPosition: CameraPosition(
-          target: _center,
-          zoom: 11.0,
-        ),
-        markers: markers,
-      ),
-    );
+    return StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection("sucursales").snapshots(),
+        builder: (BuildContext context,
+            AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+          if (streamSnapshot.hasData) {
+            var puntos = streamSnapshot.data!.docs
+                .map((doc) => Marker(
+                    markerId: MarkerId(doc['nombre']),
+                    icon: BitmapDescriptor.defaultMarkerWithHue(
+                        BitmapDescriptor.hueAzure),
+                    infoWindow: InfoWindow(
+                        title: doc['nombre'], snippet: doc['descripcion']),
+                    position: LatLng(
+                        doc['ubicacion'].latitude, doc['ubicacion'].longitude)))
+                .toList();
+
+            return Scaffold(
+              appBar: AppBar(
+                  title: const Text('Maps App'),
+                  backgroundColor: Colors.amber.shade700),
+              body: GoogleMap(
+                onMapCreated: _onMapCreated,
+                initialCameraPosition: CameraPosition(
+                  target: _center,
+                  zoom: 11.0,
+                ),
+                markers: Set.from(puntos),
+              ),
+            );
+          }
+
+          return const Center(child: CircularProgressIndicator());
+        });
   }
 }
